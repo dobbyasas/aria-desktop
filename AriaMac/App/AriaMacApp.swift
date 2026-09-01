efae807f1de7c8fc8534c82a56979a6785cf3814
dobbyasas <svejkovsky.kry@gmail.com>
2026-09-01 21:46:30 +0200

    Advance shared release to 1.15.0

--- AriaMac/App/AriaMacApp.swift
+++ AriaMac/App/AriaMacApp.swift
@@ -4,7 +4,7 @@ import SwiftUI
 enum AriaRelease {
     static let version = Bundle.main.object(
         forInfoDictionaryKey: "CFBundleShortVersionString"
-    ) as? String ?? "1.14.4"
+    ) as? String ?? "1.15.0"
 
     static var displayText: String {
         "Version \(version)"
