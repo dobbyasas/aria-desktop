b778113fec67ee359050f1a4a93e9976921188f7
dobbyasas <svejkovsky.kry@gmail.com>
2026-09-10 14:15:39 +0200

    perf: make Mac song scrolling smooth in 1.22.3

--- AriaMac/App/AriaMacApp.swift
+++ AriaMac/App/AriaMacApp.swift
@@ -4,7 +4,7 @@ import SwiftUI
 enum AriaRelease {
     static let version = Bundle.main.object(
         forInfoDictionaryKey: "CFBundleShortVersionString"
-    ) as? String ?? "1.22.2"
+    ) as? String ?? "1.22.3"
 
     static var displayText: String {
         "Version \(version)"
